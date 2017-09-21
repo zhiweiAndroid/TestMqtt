ad315f6aeb7d2b3949b28518ab8e309df3a87c92
zhiwei <deva1213082309@gmail.com>
2017-09-21 16:57:52 +0800

    mainactivity3

--- app/src/main/java/com/example/shundai/testmqtt/Main3Activity.java
+++ app/src/main/java/com/example/shundai/testmqtt/Main3Activity.java
@@ -3,12 +3,11 @@ package com.example.shundai.testmqtt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-
+        setContentView(R.layout.activity_main3);
     }
 }
